b4e748851db37f9a5cba607a82615ccede8fbad5
lruzicki <lruzicki@gmail.com>
2023-06-19 13:28:15 +0200

    TECH-717: draft version for new adaptor module

---
+++ README.md
@@ -0,0 +1,25 @@
+# openIMIS Backend Report reference module
+This repository holds the files of the openIMIS Backend Core reference module.
+It is a required module of [openimis-be_py](https://github.com/openimis/openimis-be_py).
+
+[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)
+
+## ORM mapping:
+* 
+
+## Listened Django Signals
+None
+
+## Services
+
+## GraphQL Queries
+None
+
+## GraphQL Mutations - each mutation emits default signals and return standard error lists (cfr. openimis-be-core_py)
+None
+
+## Configuration options (can be changed via core.ModuleConfiguration)
+None
+
+## openIMIS Modules Dependencies
+* core.models.UUIDModel
---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='openimis-be-govstack_test_harness_api_py',
+    version='1.0.0.rc1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='GNU AGPL v3',
+    description='The openIMIS API for test harness.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://openimis.org/',
+    author='Łukasz Ruzicki',
+    author_email='lruzicki@soldevelo.com',
+    install_requires=[
+        'django',
+        'django-db-signals',
+        'djangorestframework',
+        'openimis-be-core',
+        'openimis-be-location',
+        'openimis-be-payment',
+        'openimis-be-policy',
+        'openimis-be-insuree'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
