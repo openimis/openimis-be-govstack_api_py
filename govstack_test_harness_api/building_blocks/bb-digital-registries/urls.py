cac54338ff21893dffcc8bf8f7ae4d2e36bdd3bc
lruzicki <lruzicki@gmail.com>
2023-06-20 20:28:34 +0200

    draft

--- govstack_test_harness_api/building_blocks/bb-digital-registries/urls.py
+++ govstack_test_harness_api/building_blocks/bb-digital-registries/urls.py
@@ -4,5 +4,5 @@ from .views import getDataFromSubFolder, get_registry_data, get_registry_data2
 urlpatterns = [
     path('bbdr/', getDataFromSubFolder, name='digital_registries'),
     path('data/<str:registryname>/<str:versionnumber>/exists', get_registry_data, name='data_registry'),
-    path('data/<str:registryname>/<str:versionnumber>/exists2', get_registry_data2, name='data_registry'),
+    path('data/<str:registryname>/<str:versionnumber>/exists2', get_registry_data2, name='data_registry2'),
 ]
