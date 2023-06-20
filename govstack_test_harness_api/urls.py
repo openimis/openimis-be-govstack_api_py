935174f55ce4d32e3df1ddaaa8e2a4a28b269e00
lruzicki <lruzicki@gmail.com>
2023-06-20 15:47:37 +0200

    TECH-717: added init for each folder

--- govstack_test_harness_api/urls.py
+++ govstack_test_harness_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
+import govstack_test_harness_api
 
 urlpatterns = [
-    # path('', include('govstack_test_harness_api.building_blocks.bb-digital-registries.urls')),
+    path('', include('govstack_test_harness_api.building_blocks.bb-digital-registries.urls')),
 ]
