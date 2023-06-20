e5b7a6363ba2bb5e86d3e49c8d6a127564993f61
lruzicki <lruzicki@gmail.com>
2023-06-20 20:52:05 +0200

    deleted build

--- build/lib/govstack_test_harness_api/building_blocks/bb-digital-registries/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import getDataFromSubFolder, get_registry_data
-
-urlpatterns = [
-    path('bbdr/', getDataFromSubFolder, name='digital_registries'),
-    path('data/<str:registryname>/<str:versionnumber>/exists', get_registry_data, name='data_registry'),
-]
