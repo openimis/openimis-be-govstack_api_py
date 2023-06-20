e0aa4f1b066e6cf314a8581f783712fac9334ce1
lruzicki <lruzicki@gmail.com>
2023-06-20 11:21:26 +0200

    REVERTED

--- govstack_test_harness_api/urls.py
+++ govstack_test_harness_api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('gov', include('govstack_test_harness_api.building_blocks.bb-digital-registries.urls')),
+    path('', include('govstack_test_harness_api.building_blocks.bb-digital-registries.urls')),
 ]
