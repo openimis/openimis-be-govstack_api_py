821d0063c9e4c8d3d2cce766c5eeadf773999b4f
lruzicki <lruzicki@gmail.com>
2023-07-18 11:52:25 +0200

    TECH-717: module renamed to govstack_api

---
+++ govstack_api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', include('govstack_api.building_blocks.bb-digital-registries.urls')),
+]
--- govstack_test_harness_api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', include('govstack_test_harness_api.building_blocks.bb-digital-registries.urls')),
-]
